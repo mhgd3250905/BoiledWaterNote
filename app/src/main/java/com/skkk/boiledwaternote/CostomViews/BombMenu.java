6d8a117547f55ec83a506071e06ca87a294bed4c
ksheng <294851575@qq.com>
2017-06-28 22:53:40 +0800

    1.完成弹射菜单，感觉还不错

---
+++ app/src/main/java/com/skkk/boiledwaternote/CostomViews/BombMenu.java
@@ -0,0 +1,257 @@
+package com.skkk.boiledwaternote.CostomViews;
+
+/**
+ * 创建于 2017/6/28
+ * 作者 admin
+ */
+/*
+* 
+* 描    述：
+* 作    者：ksheng
+* 时    间：2017/6/28$ 21:58$.
+*/
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+
+/**
+ * 创建于 2017/6/28
+ * 作者 admin
+ */
+/*
+*
+* 描    述：弹射菜单
+* 作    者：ksheng
+* 时    间：2017/6/28$ 20:50$.
+*/
+public class BombMenu  extends ViewGroup implements View.OnClickListener {
+
+    private final String TAG=BombMenu.class.getSimpleName();
+    private final int POS_LEFT_TOP=0;
+    private final int POS_LEFT_BUTTOM=1;
+    private final int POS_RIGHT_TOP=2;
+    private final int POS_RIGHT_BUTTOM=3;
+
+    private final int STATUS_CLOSE=0;
+    private final int STATUS_OPEN=1;
+
+    private final int DIRECTION_HORIZONTAL=0;
+    private final int DIRECTION_VERTICAL=1;
+
+    private int mPosition=POS_RIGHT_TOP;
+    private int mStatus=STATUS_CLOSE;
+    private int mDirection=DIRECTION_VERTICAL;
+
+    private View mainButton;
+
+    private OnMenuItemClickListener mMenuItemClickListener;
+    private int width;
+    private int height;
+    private long animDuration=300;
+
+    public void setmMenuItemClickListener(OnMenuItemClickListener mMenuItemClickListener) {
+        this.mMenuItemClickListener = mMenuItemClickListener;
+    }
+
+    public BombMenu(Context context) {
+        super(context);
+    }
+
+    public BombMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BombMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initAttrs();
+    }
+
+    /**
+     * 注册自定义属性
+     */
+    private void initAttrs() {
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count=getChildCount();
+        for (int i = 0; i < count; i++) {
+            measureChild(getChildAt(i),widthMeasureSpec,heightMeasureSpec);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed){
+            layoutButton();
+        }
+    }
+
+    /**
+     * 布局按钮
+     */
+    private void layoutButton() {
+        /*
+        * 计算所有按钮的位置
+        * */
+        int count=getChildCount();
+        mainButton=getChildAt(count-1);
+
+        int l=0;
+        int t=0;
+        width = mainButton.getMeasuredWidth();
+        height = mainButton.getMeasuredHeight();
+
+        switch (mPosition){
+            case POS_LEFT_TOP:
+                break;
+            case POS_LEFT_BUTTOM:
+                t=getMeasuredHeight()- height;
+                break;
+            case POS_RIGHT_TOP:
+                l=getMeasuredWidth()- width;
+                break;
+            case POS_RIGHT_BUTTOM:
+                l=getMeasuredWidth()- width;
+                t=getMeasuredHeight()- height;
+                break;
+        }
+        /*
+        * 放置菜单按钮
+        * */
+        mainButton.layout(l,t,l+ width,t+ height);
+        mainButton.setClickable(true);
+        mainButton.setFocusable(true);
+        mainButton.setOnClickListener(this);
+        /*
+        * 放置子菜单
+        * */
+        for (int i = 0; i < count-1; i++) {
+            View childView=getChildAt(i);
+            childView.layout(l,t,l+ width,t+ height);
+            childView.setVisibility(GONE);
+            final int pos=i;
+            childView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickItemAnim(pos);
+                    changeStatus();
+                    mMenuItemClickListener.onItemClickListener(pos,v);
+                }
+            });
+        }
+
+
+    }
+
+
+    /**
+     * Item点击动画
+     * @param pos
+     */
+    private void clickItemAnim(int pos) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        openMainButton();
+        toggleMenu();
+    }
+
+    /**
+     * 展开菜单
+     */
+    private void toggleMenu() {
+        int itemCount=getChildCount()-1;
+        for (int i = 0; i < itemCount; i++) {
+            final View child=getChildAt(i);
+            child.setVisibility(VISIBLE);
+            child.setClickable(true);
+            child.setFocusable(true);
+
+            /*
+            * 计算出子按钮位移的距离
+            * */
+            int x=0;
+            int y=0;
+            if (mDirection==DIRECTION_HORIZONTAL){  //水平平移
+                x=(i+1)*width;
+            }else if (mDirection==DIRECTION_VERTICAL){  //垂直平移
+                y=(i+1)*height;
+            }
+            /*
+            * 判断位移方向
+            * */
+            int xFlag=1;
+            int yFlag=1;
+            if (mPosition==POS_RIGHT_TOP||mPosition==POS_RIGHT_BUTTOM){
+                xFlag=-1;
+            }
+            if (mPosition==POS_LEFT_BUTTOM||mPosition==POS_RIGHT_BUTTOM) {
+                xFlag=-1;
+            }
+
+            ObjectAnimator transXAnimator=null;
+            ObjectAnimator transYAnimator=null;
+            AnimatorSet animatorSet = new AnimatorSet();
+            if (mStatus==STATUS_CLOSE){     //设置弹出动画
+
+                animatorSet.setInterpolator(new BounceInterpolator());
+                animatorSet.setDuration(animDuration);
+
+                transXAnimator=ObjectAnimator.ofFloat(child,"translationX",
+                        0,x*xFlag);
+                transYAnimator=ObjectAnimator.ofFloat(child,"translationY",
+                        0,y*yFlag);
+            }else {     //设置回收动画
+                animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+                animatorSet.setDuration(animDuration*2/3);
+
+                transXAnimator=ObjectAnimator.ofFloat(child,"translationX",
+                        x*xFlag,0);
+                transYAnimator=ObjectAnimator.ofFloat(child,"translationY",
+                        y*yFlag,0);
+            }
+            animatorSet.play(transXAnimator).with(transYAnimator);
+
+            animatorSet.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if (mStatus==STATUS_CLOSE){
+                        child.setVisibility(GONE);
+                        child.setClickable(false);
+                        child.setFocusable(false);
+                    }
+                    super.onAnimationEnd(animation);
+                }
+            });
+            animatorSet.start();
+        }
+        changeStatus();
+    }
+
+    /**
+     * 切换状态
+     */
+    private void changeStatus() {
+        mStatus=(mStatus==STATUS_CLOSE?STATUS_OPEN:STATUS_CLOSE);
+    }
+
+    /**
+     * 打开主菜单
+     */
+    private void openMainButton() {
+
+    }
+}
+
---
+++ app/src/main/java/com/skkk/boiledwaternote/CostomViews/OnMenuItemClickListener.java
@@ -0,0 +1,12 @@
+package com.skkk.boiledwaternote.CostomViews;
+
+import android.view.View;
+
+/**
+ * 创建于 2017/6/28
+ * 作者 admin
+ */
+
+public interface OnMenuItemClickListener {
+    void onItemClickListener(int pos, View v);
+}
